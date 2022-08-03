92c38e718336e543609c38944ddaf40b04843be0
Kyle R <pacman611@hotmail.com>
2022-08-03 11:32:11 -0500

    first-commit

---
+++ src/TodoList.js
@@ -0,0 +1,12 @@
+function TodoList() {
+  return (
+    <div className="todoListMain">
+      <form>
+        <input placeholder="Task" />
+        <button type="submit">Add Task</button>
+      </form>
+    </div>
+  );
+}
+
+export default TodoList;
